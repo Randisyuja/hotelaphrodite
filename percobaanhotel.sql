72a25586d9c8cf05b36c899564933ca459b27a05
Randisyuja <99237323+Randisyuja@users.noreply.github.com>
2022-12-07 17:41:43 +0700

    Add files via upload

--- percobaanhotel.sql
+++ percobaanhotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2022 at 04:54 PM
+-- Generation Time: Dec 07, 2022 at 11:37 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -45,7 +45,8 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`nik`, `name`, `email`, `password`, `address`, `city`, `religion`, `sex`, `nohp`) VALUES
 ('321325354377657', 'Kamu Nanyea', 'randijordan@gmai.com', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'Senopati', 'Jakarta', 'Islam', 'Male', '081212121212'),
-('3216181312020001', 'Randi Syuja', 'syuja131202@gmail.com', '6a204bd89f3c8348afd5c77c717a097a', 'citayeum', 'jakarta', 'Islam', 'Male', '081298274008');
+('3216181312020001', 'Randi Syuja', 'syuja131202@gmail.com', '6a204bd89f3c8348afd5c77c717a097a', 'citayeum', 'jakarta', 'Islam', 'Male', '081298274008'),
+('3216181312020007', 'mutia khoirunniza', 'mutia21@gmail.com', '90266c389027d3c9b3fc57247f5b0af2', 'Setu', 'bandung', 'Protestant', 'Female', '081298274007');
 
 -- --------------------------------------------------------
 
@@ -71,7 +72,29 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `nik`, `name`, `email`, `password`, `address`, `city`, `religion`, `sex`, `nohp`) VALUES
-(2, '3216181312020001', 'Randi Syuja', 'syuja131202@gmail.com', '6a204bd89f3c8348afd5c77c717a097a', 'citayeum', 'jakarta', 'Islam', 'Male', '081298274008');
+(2, '3216181312020001', 'Randi Syuja', 'syuja131202@gmail.com', '6a204bd89f3c8348afd5c77c717a097a', 'citayeum', 'jakarta', 'Islam', 'Male', '081298274008'),
+(3, '32161813120200098', 'diah ayu', 'randijordan0@gmail.com', 'b1980b34d5180cf2051d0fe400cb86e0', 'setu', 'Bekasi', 'Islam', 'Female', '081298274098');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `checkin`
+--
+
+CREATE TABLE `checkin` (
+  `no_pemesanan` int(11) NOT NULL,
+  `branch` varchar(100) NOT NULL,
+  `kode_kamar` char(5) NOT NULL,
+  `tipe_kamar` varchar(100) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `checkin` date NOT NULL,
+  `checkout` date NOT NULL,
+  `lama_menginap` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `nik` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nohp` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -80,6 +103,8 @@ INSERT INTO `admin` (`id`, `nik`, `name`, `email`, `password`, `address`, `city`
 --
 
 CREATE TABLE `client` (
+  `no_pemesanan` int(11) NOT NULL,
+  `branch` varchar(100) NOT NULL,
   `kode_kamar` char(5) NOT NULL,
   `tipe_kamar` varchar(100) NOT NULL,
   `harga` int(11) NOT NULL,
@@ -87,6 +112,7 @@ CREATE TABLE `client` (
   `checkout` date NOT NULL,
   `lama_menginap` int(11) NOT NULL,
   `total` int(11) NOT NULL,
+  `nik` varchar(100) NOT NULL,
   `nama` varchar(100) NOT NULL,
   `nohp` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -95,11 +121,8 @@ CREATE TABLE `client` (
 -- Dumping data for table `client`
 --
 
-INSERT INTO `client` (`kode_kamar`, `tipe_kamar`, `harga`, `checkin`, `checkout`, `lama_menginap`, `total`, `nama`, `nohp`) VALUES
-('', '', 0, '0000-00-00', '0000-00-00', 0, 0, 'Randi Syuja', ''),
-('0001', 'Double Bed Room', 499000, '2022-12-21', '2022-12-24', 3, 1497000, 'Randi Syuja', '081298274008'),
-('db01', 'Double Bed Room', 750000, '2022-12-02', '2022-12-03', 2, 1500000, 'Randi', '089676767878'),
-('db02', 'Double Bed Room 4', 768888, '2022-12-15', '2022-12-17', 2, 1537776, 'Randi Syuja', '081298274008');
+INSERT INTO `client` (`no_pemesanan`, `branch`, `kode_kamar`, `tipe_kamar`, `harga`, `checkin`, `checkout`, `lama_menginap`, `total`, `nik`, `nama`, `nohp`) VALUES
+(10, 'indonesia', 'db01', 'Double Bed Room', 1199000, '2022-12-15', '2022-12-17', 2, 2398000, '3216181312020007', 'mutia khoirunniza', '081298274007');
 
 -- --------------------------------------------------------
 
@@ -120,10 +143,9 @@ CREATE TABLE `indonesia` (
 --
 
 INSERT INTO `indonesia` (`kode_kamar`, `image`, `tipe_kamar`, `deskripsi`, `harga`) VALUES
-('db02', 'baliei.jpg', 'Double Bed Room 4', 'BALI23', 768888),
-('db03', 'doubleus.jpg', 'Double Bed Room 2', 'anjaymabar', 1290000),
-('sb01', 'kamarjapan4.jpeg', 'Single Bed Room', 'asdfadf', 359999),
-('tb02', 'twbaliee.jpg', 'Twin Bed Room', 'book sekarang juga boy', 649000);
+('db01', 'baliei.jpg', 'Double Bed Room', '**** 9,0 Luar Biasa ( 2,250 ulasan )\r\n\r\nHarga per malam mulai dari  ', 1199000),
+('tb2', 'balie2.jpg', 'Twins Bed Room', '**** 8,8 Luar Biasa ( 2,240 ulasan) \r\nharga per malam mulai dari ', 1150000),
+('tb4', 'balie1.jpg', 'Single Bed Room', '**** 9,9 Luar Biasa ( 4,990 Ulasan )\r\nharga per malam mulai dari ', 1000000);
 
 -- --------------------------------------------------------
 
@@ -144,18 +166,19 @@ CREATE TABLE `japan` (
 --
 
 INSERT INTO `japan` (`kode_kamar`, `image`, `tipe_kamar`, `deskripsi`, `harga`) VALUES
-('0001', 'twbaliee.jpg', 'Double Bed Room', 'assadadfa', 499000),
-('0002', 'twin us.jpg', 'Twin Bed Room', 'anjay mabar', 349000);
+('jptb', 'kamarjapan.jpeg', 'Twin Bed Room', '**** 9,9 Luar Biasa (5,250 Ulasan )\r\nHarga Per Malam mulai dari ', 2300000),
+('jpsb', 'kamarjapan4.jpeg', 'Single Bed Room', '**** 9,5 Luar Biasa ( 3,120 Ulasan )\r\nHarga Per Malam mulai dari \r\n', 2000000),
+('jpdb', 'kamarjapan2.jpeg', 'Double Bed Room', '**** 8,5 Luar Biasa ( 4,540 ulasan) \r\nHarga Per Malam mulai dari \r\n', 3000000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `room_type`
+-- Table structure for table `no_room`
 --
 
-CREATE TABLE `room_type` (
-  `roomtype` varchar(50) NOT NULL,
-  `harga` int(11) NOT NULL
+CREATE TABLE `no_room` (
+  `noroom` int(50) NOT NULL,
+  `kode_kamar` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -177,7 +200,9 @@ CREATE TABLE `swiss` (
 --
 
 INSERT INTO `swiss` (`kode_kamar`, `image`, `tipe_kamar`, `deskripsi`, `harga`) VALUES
-('fr05', 'swiss.jpeg', 'Family Room', 'suit for family time', 999000);
+('swsb', 'swiss2.jpg', 'Single Bed Room', '**** 9,0 Luar Biasa ( 2,240 ulasan) \r\nHarga Per malam mulai dari ', 2900000),
+('swtb', 'swiss3.jpg', 'Twin Bed Room', '**** 9,6 Luar Biasa ( 3,990 Ulasan )\r\nHarga Per malam mulai dari ', 3200000),
+('swbd', 'swiss1.jpg', 'Double Bed Room ', '**** 9,9 Luar Biasa ( 5,000 Ulasan )\r\nHarga Per malam mulai dari ', 3500000);
 
 --
 -- Indexes for dumped tables
@@ -200,7 +225,8 @@ ALTER TABLE `admin`
 -- Indexes for table `client`
 --
 ALTER TABLE `client`
-  ADD PRIMARY KEY (`kode_kamar`);
+  ADD PRIMARY KEY (`no_pemesanan`),
+  ADD UNIQUE KEY `nik` (`nik`);
 
 --
 -- Indexes for table `indonesia`
@@ -216,7 +242,13 @@ ALTER TABLE `indonesia`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `no_pemesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
